3394c190159b2a40183a879ee30adff116072281
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-12-06 23:53:40 -0500

    feat: adds basic hit and hurt boxes

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -8,7 +8,14 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
 
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(hurt)
+
+
+func hurt(hitbox: HitboxComponent):
+	queue_free()
